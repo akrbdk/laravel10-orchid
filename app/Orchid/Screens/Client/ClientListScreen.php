12e2c3ea3cc0052de50fcb78c75622047412642d
alexander.karabudka <sanyakarabudka@gmail.com>
2023-08-14 22:57:00 +0300

    test layout

--- app/Orchid/Screens/Client/ClientListScreen.php
+++ app/Orchid/Screens/Client/ClientListScreen.php
@@ -4,6 +4,8 @@ namespace App\Orchid\Screens\Client;
 
 use App\Models\Client;
 use Orchid\Screen\Screen;
+use Orchid\Screen\TD;
+use Orchid\Support\Facades\Layout;
 
 class ClientListScreen extends Screen
 {
@@ -15,7 +17,7 @@ class ClientListScreen extends Screen
     public function query(): iterable
     {
         return [
-            'clients' => Client::all()
+            'clients' => Client::paginate(20)
         ];
     }
 
@@ -49,7 +51,17 @@ class ClientListScreen extends Screen
     public function layout(): iterable
     {
         return [
-            //
+            Layout::table('clients', [
+                TD::make('name')->width('100px'),
+                TD::make('last_name')->width('100px'),
+                TD::make('status')->width('50px')->render(function (Client $client){
+                    return $client->status === 'interviewed' ? 'Обработано' : 'Необработано';
+                }),
+                TD::make('phone')->width('150px'),
+                TD::make('email')->width('100px'),
+                TD::make('birthday')->width('100px'),
+                TD::make('created_at')->defaultHidden(),
+            ])
         ];
     }
 }
